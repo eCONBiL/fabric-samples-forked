53a27e7c61459ee9628bbed574ffc934fd89c36a
maltegarmhausen <69622646+maltegarmhausen@users.noreply.github.com>
2020-10-21 16:07:00 +0200

    updated /chaincode/fabcar/go/fabcar.go (code-base)
    first update of code-base (installed on mychannel) according to BL use-case

--- chaincode/fabcar/go/fabcar.go
+++ chaincode/fabcar/go/fabcar.go
@@ -2,6 +2,11 @@
 SPDX-License-Identifier: Apache-2.0
 */
 
+/*
+modified by Malte Garmhausen
+changes made for an implementation of the Bill of Lading freight document
+*/
+
 package main
 
 import (
@@ -18,37 +23,29 @@ type SmartContract struct {
 }
 
 // Car describes basic details of what makes up a car
-type Car struct {
-	Make   string `json:"make"`
-	Model  string `json:"model"`
-	Colour string `json:"colour"`
+type BillOfLading struct {
+	Shipper   string `json:"make"`
+	DateOfIssue  string `json:"dateofissue"`
+	PlaceOfIssue string `json:"placeofissue"`
 	Owner  string `json:"owner"`
 }
 
 // QueryResult structure used for handling result of query
 type QueryResult struct {
 	Key    string `json:"Key"`
-	Record *Car
+	Record *BillOfLading
 }
 
 // InitLedger adds a base set of cars to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	cars := []Car{
-		Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko"},
-		Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad"},
-		Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo"},
-		Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max"},
-		Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana"},
-		Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel"},
-		Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav"},
-		Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari"},
-		Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria"},
-		Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro"},
-	}
+	bls := []BillOfLading{
+		BillOfLading{Shipper: "Autohaus Main GmbH", DateOfIssue: "01.01.2020", PlaceOfIssue: "Bremerhaven", Owner: "Party1"},
+		BillOfLading{Shipper: "Abat AG", DateOfIssue: "29.07.2020", PlaceOfIssue: "Bremen", Owner: "Christian"},
+		}
 
-	for i, car := range cars {
-		carAsBytes, _ := json.Marshal(car)
-		err := ctx.GetStub().PutState("CAR"+strconv.Itoa(i), carAsBytes)
+	for i, bl := range bls {
+		blAsBytes, _ := json.Marshal(bl)
+		err := ctx.GetStub().PutState("BL"+strconv.Itoa(i), blAsBytes)
 
 		if err != nil {
 			return fmt.Errorf("Failed to put to world state. %s", err.Error())
@@ -59,39 +56,39 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 }
 
 // CreateCar adds a new car to the world state with given details
-func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, carNumber string, make string, model string, colour string, owner string) error {
-	car := Car{
-		Make:   make,
-		Model:  model,
-		Colour: colour,
+func (s *SmartContract) CreateBl(ctx contractapi.TransactionContextInterface, blNumber string, shipper string, dateofissue string, placeofissue string, owner string) error {
+	bl := BillOfLading{
+		Shipper:   shipper,
+		DateOfIssue:  dateofissue,
+		PlaceOfIssue: placeofissue,
 		Owner:  owner,
 	}
 
-	carAsBytes, _ := json.Marshal(car)
+	blAsBytes, _ := json.Marshal(bl)
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+	return ctx.GetStub().PutState(blNumber, blAsBytes)
 }
 
 // QueryCar returns the car stored in the world state with given id
-func (s *SmartContract) QueryCar(ctx contractapi.TransactionContextInterface, carNumber string) (*Car, error) {
-	carAsBytes, err := ctx.GetStub().GetState(carNumber)
+func (s *SmartContract) QueryBl(ctx contractapi.TransactionContextInterface, blNumber string) (*BillOfLading, error) {
+	blAsBytes, err := ctx.GetStub().GetState(blNumber)
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
 	}
 
-	if carAsBytes == nil {
-		return nil, fmt.Errorf("%s does not exist", carNumber)
+	if blAsBytes == nil {
+		return nil, fmt.Errorf("%s does not exist", blNumber)
 	}
 
-	car := new(Car)
-	_ = json.Unmarshal(carAsBytes, car)
+	bl := new(BillOfLading)
+	_ = json.Unmarshal(blAsBytes, bl)
 
-	return car, nil
+	return bl, nil
 }
 
 // QueryAllCars returns all cars found in world state
-func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
+func (s *SmartContract) QueryAllBls(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
 	startKey := ""
 	endKey := ""
 
@@ -111,10 +108,10 @@ func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface
 			return nil, err
 		}
 
-		car := new(Car)
-		_ = json.Unmarshal(queryResponse.Value, car)
+		bl := new(BillOfLading)
+		_ = json.Unmarshal(queryResponse.Value, bl)
 
-		queryResult := QueryResult{Key: queryResponse.Key, Record: car}
+		queryResult := QueryResult{Key: queryResponse.Key, Record: bl}
 		results = append(results, queryResult)
 	}
 
@@ -122,18 +119,18 @@ func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface
 }
 
 // ChangeCarOwner updates the owner field of car with given id in world state
-func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterface, carNumber string, newOwner string) error {
-	car, err := s.QueryCar(ctx, carNumber)
+func (s *SmartContract) ChangeBlOwner(ctx contractapi.TransactionContextInterface, blNumber string, newOwner string) error {
+	bl, err := s.QueryBl(ctx, blNumber)
 
 	if err != nil {
 		return err
 	}
 
-	car.Owner = newOwner
+	bl.Owner = newOwner
 
-	carAsBytes, _ := json.Marshal(car)
+	blAsBytes, _ := json.Marshal(bl)
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+	return ctx.GetStub().PutState(blNumber, blAsBytes)
 }
 
 func main() {
@@ -141,11 +138,11 @@ func main() {
 	chaincode, err := contractapi.NewChaincode(new(SmartContract))
 
 	if err != nil {
-		fmt.Printf("Error create fabcar chaincode: %s", err.Error())
+		fmt.Printf("Error create modified fabcar chaincode: %s", err.Error())
 		return
 	}
 
 	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting fabcar chaincode: %s", err.Error())
+		fmt.Printf("Error starting modified fabcar chaincode: %s", err.Error())
 	}
 }

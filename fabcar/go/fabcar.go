8cba32ba3ebc66428d1b6561afb6c2098c118629
maltegarmhausen <69622646+maltegarmhausen@users.noreply.github.com>
2020-12-08 12:00:06 +0100

    edited create function call for v1.0
    modified cli interaction and argument passing for create-function according to the current B/L-version with 43 fields, removed the "changeOwner - cli argument", added some comments, chaincode interaction is now possible with the following function calls: create (a B/L in the ledger with the passed arguments), query (a single B/L by its BLNumber) and queryAll (query all B/Ls found in the ledger)

--- fabcar/go/fabcar.go
+++ fabcar/go/fabcar.go
@@ -3,9 +3,11 @@ Copyright 2020 IBM All Rights Reserved.
 
 fabcar.go main program containing some chaincode interaction commands for testing written BL chaincode locally
 
-edited for using command line arguments for chaincode interaction 
+edited for using command line arguments for chaincode interaction
 
 Edited by Malte Garmhausen
+
+eCONBiL - electronical Consignment and Bill of Lading
 
 SPDX-License-Identifier: Apache-2.0
 */
@@ -18,6 +20,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
@@ -67,44 +70,95 @@ func main() {
 
 	contract := network.GetContract("fabcar")
 
-	 result, err := contract.EvaluateTransaction("QueryAllBls")
-	 if err != nil {
-	 	fmt.Printf("Failed to evaluate transaction: %s\n", err)
-	 	os.Exit(1)
-	 }
+	// First query, output (var result) is currently cut out
+	result, err := contract.EvaluateTransaction("QueryAllBls")
+	if err != nil {
+		fmt.Printf("Failed to evaluate transaction: %s\n", err)
+		os.Exit(1)
+	}
 	// fmt.Println(string(result))
 
+	//the following block implements chaicode invokes and querys according to given arguments
 	if len(os.Args) > 1 {
 
 		var argument string = os.Args[1]
-		
-		if ( argument == "help" ) || ( argument == "H" ) || (argument == "Help" ) || ( argument == "h" ) {
+
+		if (argument == "help") || (argument == "H") || (argument == "Help") || (argument == "h") {
 			fmt.Println()
 			fmt.Println("Use this program to interact with deployed chaincode")
+			fmt.Println("Currently implemented functions for B/L chaincode are query, queryAll and create")
 			fmt.Println("Arguments are:")
 			fmt.Println("    queryAll")
 			fmt.Println("    query")
 			fmt.Println("    create")
-			fmt.Println("    changeOwner")
+			fmt.Println("	 help | h | Help | H")
 
-		}	
+		}
 
 		if argument == "queryAll" {
-		result, err := contract.EvaluateTransaction("queryAllBls")
-		if err != nil {
-			fmt.Printf("Failed to evaluate transaction: %s\n", err)
-			os.Exit(1)
+			result, err := contract.EvaluateTransaction("queryAllBls")
+			if err != nil {
+				fmt.Printf("Failed to evaluate transaction: %s\n", err)
+				os.Exit(1)
 			}
 			fmt.Println(string(result))
 
-		} else if  argument == "create" {
-			if len(os.Args) == 7 {
-				var createArgId string = os.Args[2]
-				var createArgSh string = os.Args[3]
-				var createArgDoi string = os.Args[4]
-				var createArgPoi string = os.Args[5]
-				var createArgOw string = os.Args[6]
-				result, err = contract.SubmitTransaction("createBl", createArgId, createArgSh, createArgDoi, createArgPoi, createArgOw)
+		} else if argument == "create" {
+			if len(os.Args) == 45 {
+				// ==> os.Args[0] = /tmp/go-build... --> default while reading arguments from os input
+				// ==> os.Args[1] = "create"
+				var createArgBlNr string = os.Args[2]
+				var createArgDoi string = os.Args[3]
+				var createArgPoi string = os.Args[4]
+				var createArgNoBl string = os.Args[5]
+				var createArgShName string = os.Args[6]
+				var createArgShAddr string = os.Args[7]
+				var createArgShCntct string = os.Args[8]
+				var createArgShLF string = os.Args[9]
+				var createArgCoName string = os.Args[10]
+				var createArgCoAddr string = os.Args[11]
+				var createArgCoCntct string = os.Args[12]
+				var createArgCoLF string = os.Args[13]
+				var createArgCrName string = os.Args[14]
+				var createArgCrAddr string = os.Args[15]
+				var createArgCrCntct string = os.Args[16]
+				var createArgCrLF string = os.Args[17]
+				var createArgCrTN string = os.Args[18]
+				var createArgACName string = os.Args[19]
+				var createArgACLF string = os.Args[20]
+				var createArgACAddr string = os.Args[21]
+				var createArgNPCName string = os.Args[22]
+				var createArgNPCAddr string = os.Args[23]
+				var createArgNPCLF string = os.Args[24]
+				var createArgNPCSame string = os.Args[25]
+				var createArgIncoterms string = os.Args[26]
+				var createArgFreightCC string = os.Args[27]
+				var createArgPrpd string = os.Args[28]
+				var createArgCllct string = os.Args[29]
+				var createArgPortOfLoading string = os.Args[30]
+				var createArgPortOfDischarge string = os.Args[31]
+				var createArgPlaceOfReceipt string = os.Args[32]
+				var createArgPlaceOfDelivery string = os.Args[33]
+				var createArgOVN string = os.Args[34]
+				var createArgContNr string = os.Args[35]
+				var createArgFullContLoad string = os.Args[36]
+				var createArgLessThenContLoad string = os.Args[37]
+				var createArgDtOfRecvd string = os.Args[38]
+				var createArgShippedOnBoardDate string = os.Args[39]
+				var createArgMrksNmbrs string = os.Args[40]
+				var createArgNrOfPkg string = os.Args[41]
+				var createArgGrossWeight string = os.Args[42]
+				var createArgGrossWeightUnit string = os.Args[43]
+				var createArgDscrOfGoods string = os.Args[44]
+
+				//Output loop for testing and monitoring purposes during development
+				for i := 0; i < 45; i++ {
+					fmt.Println("[", strconv.Itoa(i), "]", " ", os.Args[i])
+				}
+				// fmt.Println(createArgGrossWeightUnit)
+				// fmt.Println(os.Args[44])
+
+				result, err = contract.SubmitTransaction("createBl", createArgBlNr, createArgDoi, createArgPoi, createArgNoBl, createArgShName, createArgShAddr, createArgShCntct, createArgShLF, createArgCoName, createArgCoAddr, createArgCoCntct, createArgCoLF, createArgCrName, createArgCrAddr, createArgCrCntct, createArgCrLF, createArgCrTN, createArgACName, createArgACLF, createArgACAddr, createArgNPCName, createArgNPCAddr, createArgNPCLF, createArgNPCSame, createArgIncoterms, createArgFreightCC, createArgPrpd, createArgCllct, createArgPortOfLoading, createArgPortOfDischarge, createArgPlaceOfReceipt, createArgPlaceOfDelivery, createArgOVN, createArgContNr, createArgFullContLoad, createArgLessThenContLoad, createArgDtOfRecvd, createArgShippedOnBoardDate, createArgMrksNmbrs, createArgNrOfPkg, createArgGrossWeight, createArgGrossWeightUnit, createArgDscrOfGoods)
 				if err != nil {
 					fmt.Printf("Failed to submit transaction: %s\n", err)
 					os.Exit(1)
@@ -112,9 +166,15 @@ func main() {
 				fmt.Println(string(result))
 
 			} else {
-				fmt.Println("6 Arguments expected  |  go run fabcar-mod.go [create {BLID} {BLSHIPPER} {DATEOFISSUE} {PLACEOFISSUE} {BLOWNER}]")
+				fmt.Println("44 Arguments expected  |  go run fabcar.go [create {BLNumber} {DateOfIssue} {PlaceOfIssue} {NrOfBLIssued} {ShipperName} {ShipperAddress} {ShipperContact} {ShipperLegalForm} {ConsigneeName} {ConsigneeAddress} {ConsigneeContact} {ConsigneeLegalForm} {CarrierName} {CarrierAddress} {CarrierContact} {CarrierLegalForm} {CarrierTrailerNr} {AgentCompanyName} {AgentCompanyAddress} {NotifyPartyCompanyName} {NotifyPartyCompanyAddress} {NotifyPartyCompanyLegalForm} {NotifyPartySameAs} {Incoterms} {FreightChargesCurrency} {Prepaid} {Collect} {PortOfLoading} {PortOfDischarge} {PlaceOfReceipt} {PlaceOfDelivery} {OceanVesselName} {Containernumber} {FullContainerLoad} {LessThenContainerLoad} {DateOfRecieved} {ShippedOnBoardDate} {MarksAndNumbers} {NumberOfPackages} {GrossWeight} {GrossWeightUnit} {DescriptionOfGoods}]")
+
+				//Output loop for testing and monitoring purposes during development
+				// for i := 0; i < 45; i++ {
+				// 	fmt.Println("[", strconv.Itoa(i), "]", " ", os.Args[i])
+				// }
+				// fmt.Println("length of args: ", len(os.Args))
 			}
-		} else if  argument == "query" {
+		} else if argument == "query" {
 			if len(os.Args) == 3 {
 				var queryArg string = os.Args[2]
 				result, err = contract.EvaluateTransaction("queryBl", queryArg)
@@ -125,65 +185,27 @@ func main() {
 				fmt.Println(string(result))
 
 			} else {
-				fmt.Println("2 Arguments expected  |  go run fabcar-mod.go [query {BLID}]")
+				fmt.Println("2 Arguments expected  |  go run fabcar-mod.go [query {BLNumber}]")
 			}
-		} else if argument == "changeOwner" {
-			if len(os.Args) == 4 {
-				var changeArgId string = os.Args[2]
-				var changeArgOw string = os.Args[3]
-				_, err = contract.SubmitTransaction("changeBlOwner", changeArgId, changeArgOw)
-				if err != nil {
-					fmt.Printf("Failed to submit transaction: %s\n", err)
-					os.Exit(1)
-				}
-			} else {
-				fmt.Println("3 Arguments expected  |  go run fabcar-mod.go [changeOwner {BLID} {NEWBLOWNER}]")
-			}
+			// } else if argument == "changeOwner" {
+			// 	if len(os.Args) == 4 {
+			// 		var changeArgID string = os.Args[2]
+			// 		var changeArgOw string = os.Args[3]
+			// 		_, err = contract.SubmitTransaction("changeBlOwner", changeArgID, changeArgOw)
+			// 		if err != nil {
+			// 			fmt.Printf("Failed to submit transaction: %s\n", err)
+			// 			os.Exit(1)
+			// 		}
+			// 	} else {
+			// 		fmt.Println("3 Arguments expected  |  go run fabcar-mod.go [changeOwner {BLID} {NEWBLOWNER}]")
+			// 	}
 		} else {
-			fmt.Println("No valid argument | try: queryAll , create , query , changeOwner")
+			fmt.Println("No valid argument | try: queryAll , create , query, help")
 		}
 	} else {
-		fmt.Println("Missing argument(s) | try: queryAll , create , query , changeOwner")
+		fmt.Println("Missing argument(s) | try: queryAll , create , query, help")
 	}
 }
-
-/*
-	result, err = contract.SubmitTransaction("CreateBl", "CAR10", "HS Bremerhaven", "21.10.2020", "Bremerhaven", "Karin Vosseberg")
-	if err != nil {
-		fmt.Printf("Failed to submit transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
-
-	result, err = contract.EvaluateTransaction("QueryBl", "CAR10")
-	if err != nil {
-		fmt.Printf("Failed to evaluate transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
-
-	_, err = contract.SubmitTransaction("ChangeBlOwner", "CAR10", "Peter Kelb")
-	if err != nil {
-		fmt.Printf("Failed to submit transaction: %s\n", err)
-		os.Exit(1)
-	}
-
-	result, err = contract.EvaluateTransaction("QueryBl", "CAR10")
-	if err != nil {
-		fmt.Printf("Failed to evaluate transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
-
-	result, err = contract.EvaluateTransaction("QueryAllBls")
-	if err != nil {
-		fmt.Printf("Failed to evaluate transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
-}
-
-*/
 
 func populateWallet(wallet *gateway.Wallet) error {
 	credPath := filepath.Join(

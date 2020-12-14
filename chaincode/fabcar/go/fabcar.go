6663b8a7844fc2e1e4792787b03504cf7767b84f
Malte Garmhausen <mgarmhausen@outlook.de>
2020-12-14 12:34:42 +0100

    edited files to fit requirements of B/L beta v1.0

--- chaincode/fabcar/go/fabcar.go
+++ chaincode/fabcar/go/fabcar.go
@@ -2,6 +2,24 @@
 SPDX-License-Identifier: Apache-2.0
 */
 
+/*
+modified by Malte Garmhausen
+
+eCONBiL - electronical Consignment and Bill of Lading
+
+changes made for an implementation of the Bill of Lading freight document
+
+B/L chaincode Beta Version 1.0
+
+This chaincode has 43 of the collected BL fields implemented and provides functions for initializing the ledger, querying specific B/Ls, querying
+all B/Ls from the ledger as well as creating a new B/L.
+The initialization-function creates one default B/L into the ledger.
+
+Chaincode querys and invokes must be issued through the "interface program" found in /fabric-samples/fabcar/go/fabcar.go which is currently used as
+command line interface
+
+*/
+
 package main
 
 import (
@@ -12,43 +30,128 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-// SmartContract provides functions for managing a car
+// SmartContract provides functions for managing a B/L
 type SmartContract struct {
 	contractapi.Contract
 }
 
-// Car describes basic details of what makes up a car
-type Car struct {
-	Make   string `json:"make"`
-	Model  string `json:"model"`
-	Colour string `json:"colour"`
-	Owner  string `json:"owner"`
+//BillOfLading struct contains all BL-Data
+type BillOfLading struct {
+
+	// BL Info Fields
+	BLNumber         string `json:"BLNumber"`
+	DateOfIssue      string `json:"DateOfIssue"`
+	PlaceOfIssue     string `json:"PlaceOfIssue"`
+	NumberOfBLIssued int    `json:"NumberOfBLIssued"`
+
+	//Shipper information
+	ShipperName      string `json:"ShipperName"`
+	ShipperAddress   string `json:"ShipperAddress"`
+	ShipperContact   string `json:"ShipperContact"`
+	ShipperLegalForm string `json:"ShipperLegalForm"`
+
+	//Consignee information
+	ConsigneeName      string `json:"ConsigneeName"`
+	ConsigneeAddress   string `json:"ConsigneeAddress"`
+	ConsigneeContact   string `json:"ConsigneeContact"`
+	ConsigneeLegalForm string `json:"ConsigneeLegalForm"`
+
+	//Carrier information
+	CarrierName          string `json:"CarrierName"`
+	CarrierAddress       string `json:"CarrierAddress"`
+	CarrierContact       string `json:"CarrierContact"`
+	CarrierLegalForm     string `json:"CarrierLegalForm"`
+	CarrierTrailerNumber string `json:"CarrierTrailerNumber"`
+
+	//Forwarding Agent Company information
+	AgentCompanyName      string `json:"AgentCompanyName"`
+	AgentCompanyLegalForm string `json:"AgentCompanyLegalForm"`
+	AgentCompanyAddress   string `json:"AgentCompanyAddress"`
+
+	//Notify Party information
+	NotifyPartyCompanyName      string `json:"NotifyPartyCompanyName"`
+	NotifyPartyCompanyAddress   string `json:"NotifyPartyCompanyAddress"`
+	NotifyPartyCompanyLegalForm string `json:"NotifyPartyCompanyLegalForm"`
+	NotifyPartySameAs           bool   `json:"NotifyPartySameAs"`
+
+	//Term of sale information
+	Incoterms string `json:"Incoterms"`
+
+	//Basic freight information
+	FreightChargesCurrency string `json:"FreightChargesCurrency"`
+	Prepaid                bool   `json:"Prepaid"`
+	Collect                bool   `json:"Collect"`
+
+	//Transportinfo
+	PortOfLoading         string `json:"PortOfLoading"`
+	PortOfDischarge       string `json:"PortOfDischarge"`
+	PlaceOfReceipt        string `json:"PlaceOfReceipt"`
+	PlaceOfDelivery       string `json:"PlaceOfDelivery"`
+	OceanVesselName       string `json:"OceanVesselName"`
+	ContainerNumber       string `json:"ContainerNumber"`
+	FullContainerLoad     bool   `json:"FullContainerLoad"`
+	LessThenContainerLoad bool   `json:"LessThenContainerLoad"`
+	DateofReceived        string `json:"DateofReceived"`
+	ShippedOnBoardDate    string `json:"ShippedOnBoardDate"`
+
+	//Gross info
+	MarksAndNumbers    string `json:"MarksAndNumbers"`
+	NumberOfPackages   int    `json:"NumberOfPackages"`
+	GrossWeight        int    `json:"GrossWeight"`
+	GrossWeightUnit    string `json:"GrossWeightUnit"`
+	DescriptionOfGoods string `json:"DescriptionOfGoods"`
+
+	/* removed this fields temporarily because of argument limitation issue in create function */
+	// 	DescriptionPerPackage      int    `json:"DescriptionPerPackage"`
+	// 	Measurement                int    `json:"Measurement"`
+	// 	MeasurementUnit            string `json:"MeasurementUnit"`
+	// 	DeclaredCargoValueAmount   int    `json:"DeclaredCargoValueAmount"`
+	// 	DeclaredCargoValueCurrency string `json:"DeclaredCargoValueCurrency"`
+	// 	AdditionalInformation      string `json:"AdditionalInformation"`
+	// 	HazardousMaterial          bool `json:"HazardousMaterial"`
+
+	// // Rest
+	// 	CustomerOrderNumber int `json:"CustomerOrderNumber"`
+
+	//Used Conditions (ERA600, Art. 20 a)
+	// 	TransportConditions string `json:"TransportConditions"`
+	// 	ApplieableLaw	string `json:"ApplieableLaw"`
+	// 	PlaceOfJurisdiction string `json:"PlaceOfJurisdiction"`
+
+	//Endorsement info
+	// 	endorsement information still missing, needs to be implemented later
+
 }
 
 // QueryResult structure used for handling result of query
 type QueryResult struct {
 	Key    string `json:"Key"`
-	Record *Car
+	Record *BillOfLading
 }
 
-// InitLedger adds a base set of cars to the ledger
+// InitLedger adds a base set of BL to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	cars := []Car{
-		Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko"},
-		Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad"},
-		Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo"},
-		Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max"},
-		Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana"},
-		Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel"},
-		Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav"},
-		Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari"},
-		Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria"},
-		Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro"},
+	bls := []BillOfLading{
+		BillOfLading{BLNumber: "TW ECON 1000", DateOfIssue: "10.11.2020", PlaceOfIssue: "Bremerhaven", NumberOfBLIssued: 2,
+			ShipperName: "Autohaus Main GmbH", ShipperAddress: "Hanauerlandstr. 3460314 Frankfurt, Germany", ShipperContact: "ahmain@beispiel.de", ShipperLegalForm: "GmbH",
+			ConsigneeName: "German-Cars Ldt.", ConsigneeAddress: "Fue Avenue, A1 518108 Shanghai, China", ConsigneeContact: "86282452253", ConsigneeLegalForm: "Ldt.",
+			CarrierName: "MSC Germany S.A. & Co. KG", CarrierAddress: "Hafenstraße 55, 282127 Bremen, Germany", CarrierContact: "deu-bremen@msc.de", CarrierLegalForm: "S.A. & Co. KG", CarrierTrailerNumber: "HB-KK-596",
+			AgentCompanyName: "BLG AutoTerminal Bremerhaven GmbH & Co. KG", AgentCompanyLegalForm: "GmbH & Co. KG", AgentCompanyAddress: "Senator-Borttscheller-Str. 1, 27568 Bremerhaven, Germany",
+			NotifyPartyCompanyName: "German-Cars Ldt.", NotifyPartyCompanyAddress: "Fue Avenue, A1 518108 Shanghai, China", NotifyPartyCompanyLegalForm: "Ldt.", NotifyPartySameAs: true,
+			Incoterms:              "FOB (2020)",
+			FreightChargesCurrency: "USD", Prepaid: true, Collect: true,
+			PortOfLoading: "Bremerhaven Containerterminal", PortOfDischarge: "Shanghai Yangshan", PlaceOfReceipt: "Frankfurt am Main, Adresse, Germany", PlaceOfDelivery: "Shanghai, Adresse, China", OceanVesselName: "MSC Gulsun", ContainerNumber: "OOLU1548378", FullContainerLoad: true, LessThenContainerLoad: false, DateofReceived: "08.02.2020", ShippedOnBoardDate: "09.02.2020",
+			MarksAndNumbers: "40' steel Dry Cargo Container No CSQU3054383", NumberOfPackages: 15, GrossWeight: 4250, GrossWeightUnit: "Kg", DescriptionOfGoods: "engines and fitting engine parts packaged together on pallets", /*DescriptionPerPackage: 1, Measurement: 40, MeasurementUnit: "Feet", DeclaredCargoValueAmount: 75000, DeclaredCargoValueCurrency: "USD", AdditionalInformation: "-", HazardousMaterial: false, */
+			//CustomerOrderNumber: 1,
+			//Information about used conditions
+			//Endorsement Information
+
+		},
 	}
 
-	for i, car := range cars {
-		carAsBytes, _ := json.Marshal(car)
-		err := ctx.GetStub().PutState("CAR"+strconv.Itoa(i), carAsBytes)
+	for i, bl := range bls {
+		blAsBytes, _ := json.Marshal(bl)
+		err := ctx.GetStub().PutState("TW ECON 100"+strconv.Itoa(i), blAsBytes)
 
 		if err != nil {
 			return fmt.Errorf("Failed to put to world state. %s", err.Error())
@@ -58,40 +161,142 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 	return nil
 }
 
-// CreateCar adds a new car to the world state with given details
-func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, carNumber string, make string, model string, colour string, owner string) error {
-	car := Car{
-		Make:   make,
-		Model:  model,
-		Colour: colour,
-		Owner:  owner,
+// CreateBl adds a new BL to the world state with given details
+func (s *SmartContract) CreateBl(ctx contractapi.TransactionContextInterface, aBLNumber string, aDateOfIssue string, aPlaceOfIssue string, aNumberOfBLIssued string, aShipperName string, aShipperAddress string, aShipperContact string, aShipperLegalForm string,
+	aConsigneeName string, aConsigneeAddress string, aConsigneeContact string, aConsigneeLegalForm string,
+	aCarrierName string, aCarrierAddress string, aCarrierContact string, aCarrierLegalForm string, aCarrierTrailerNumber string,
+	aAgentCompanyName string, aAgentCompanyLegalForm string, aAgentCompanyAddress string, aNotifyPartyCompanyName string,
+	aNotifyPartyCompanyAddress string, aNotifyPartyCompanyLegalForm string, aNotifyPartySameAs string, aIncoterms string,
+	aFreightChargesCurrency string, aPrepaid string, aCollect string, aPortOfLoading string, aPortOfDischarge string, aPlaceOfReceipt string, aPlaceOfDelivery string,
+	aOceanVesselName string, aContainerNumber string, aFullContainerLoad string, aLessThenContainerLoad string, aDateofReceived string,
+	aShippedOnBoardDate string, aMarksAndNumbers string, aNumberOfPackages string, aGrossWeight string, aGrossWeightUnit string, aDescriptionOfGoods string,
+	/* aDescriptionPerPackage int, aMeasurement int, aMeasurementUnit string, aDeclaredCargoValueAmount int, aDeclaredCargoValueCurrency string,
+	   aAdditionalInformation string, aHazardousMaterial bool, aCustomerOrderNumber int*/) error {
+
+	// converting passed arguments from string to correct data type in the following block
+
+	convNumberBlIssued, converr := strconv.Atoi(aNumberOfBLIssued)
+	if converr != nil {
+		return fmt.Errorf("Error while converting aNumberOfBLIssued to int", converr.Error())
 	}
 
-	carAsBytes, _ := json.Marshal(car)
+	convNumberOfPackages, converr := strconv.Atoi(aNumberOfPackages)
+	if converr != nil {
+		return fmt.Errorf("Error while converting aNumberOfPackages to int", converr.Error())
+	}
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+	convGrossWeight, converr := strconv.Atoi(aGrossWeight)
+	if converr != nil {
+		return fmt.Errorf("Error while converting aGrossWeight to int", converr.Error())
+	}
+
+	convNotifyPartySameAs, converr := strconv.ParseBool(aNotifyPartySameAs)
+	if converr != nil {
+		return fmt.Errorf("Error while converting aNotifyPartySameAs to bool", converr.Error())
+	}
+
+	convPrepaid, converr := strconv.ParseBool(aPrepaid)
+	if converr != nil {
+		return fmt.Errorf("Error while converting aPrepaid to bool", converr.Error())
+	}
+
+	convCollect, converr := strconv.ParseBool(aCollect)
+	if converr != nil {
+		return fmt.Errorf("Error while converting aCollect to bool", converr.Error())
+	}
+
+	convFullContainerLoad, converr := strconv.ParseBool(aFullContainerLoad)
+	if converr != nil {
+		return fmt.Errorf("Error while converting aFullContainerLoad to bool", converr.Error())
+	}
+
+	convLessThenContainerLoad, converr := strconv.ParseBool(aLessThenContainerLoad)
+	if converr != nil {
+		return fmt.Errorf("Error while converting aLessThenContainerLoad to bool", converr.Error())
+	}
+
+	// creation of new bl with all fields being initialization of the passed arguments as field data after some fields data type has been converted
+	bl := BillOfLading{
+		BLNumber:                    aBLNumber,
+		DateOfIssue:                 aDateOfIssue,
+		PlaceOfIssue:                aPlaceOfIssue,
+		NumberOfBLIssued:            convNumberBlIssued, //with converted datatype atoi (string to int)
+		ShipperName:                 aShipperName,       //5
+		ShipperAddress:              aShipperAddress,
+		ShipperContact:              aShipperContact,
+		ShipperLegalForm:            aShipperLegalForm,
+		ConsigneeName:               aConsigneeName,
+		ConsigneeAddress:            aConsigneeAddress, //10
+		ConsigneeContact:            aConsigneeContact,
+		ConsigneeLegalForm:          aConsigneeLegalForm,
+		CarrierName:                 aCarrierName,
+		CarrierAddress:              aCarrierAddress,
+		CarrierContact:              aCarrierContact, //15
+		CarrierLegalForm:            aCarrierLegalForm,
+		CarrierTrailerNumber:        aCarrierTrailerNumber,
+		AgentCompanyName:            aAgentCompanyName,
+		AgentCompanyLegalForm:       aAgentCompanyLegalForm,
+		AgentCompanyAddress:         aAgentCompanyLegalForm, //20
+		NotifyPartyCompanyName:      aNotifyPartyCompanyName,
+		NotifyPartyCompanyAddress:   aNotifyPartyCompanyAddress,
+		NotifyPartyCompanyLegalForm: aNotifyPartyCompanyLegalForm,
+		NotifyPartySameAs:           convNotifyPartySameAs, // with converted datatype parseBool (string to bool)
+		Incoterms:                   aIncoterms,            //25
+		FreightChargesCurrency:      aFreightChargesCurrency,
+		Prepaid:                     convPrepaid, // with converted datatype ParseBool (string to bool)
+		Collect:                     convCollect, // with converted datatype ParseBool (string to bool)
+		PortOfLoading:               aPortOfLoading,
+		PortOfDischarge:             aPortOfDischarge, //30
+		PlaceOfReceipt:              aPlaceOfReceipt,
+		PlaceOfDelivery:             aPlaceOfDelivery,
+		OceanVesselName:             aOceanVesselName,
+		ContainerNumber:             aContainerNumber,
+		FullContainerLoad:           convFullContainerLoad,     //35	 with converted datatype ParseBool (string to bool)
+		LessThenContainerLoad:       convLessThenContainerLoad, // with converted datatype ParseBool (string to bool)
+		DateofReceived:              aDateofReceived,
+		ShippedOnBoardDate:          aShippedOnBoardDate,
+		MarksAndNumbers:             aMarksAndNumbers,
+		NumberOfPackages:            convNumberOfPackages, //40		with converted datatype Atoi (string to int)
+		GrossWeight:                 convGrossWeight,      // with converted datatype Atoi (string to int)
+		GrossWeightUnit:             aGrossWeightUnit,
+		DescriptionOfGoods:          aDescriptionOfGoods,
+
+		/* removed these fields temporarily because of arugument limitation issue --> 47 args max */
+		// DescriptionPerPackage: aDescriptionPerPackage,
+		// Measurement: aMeasurement,
+		// MeasurementUnit: aMeasurementUnit,
+		// DeclaredCargoValueAmount: aDeclaredCargoValueAmount,
+		// DeclaredCargoValueCurrency: aDeclaredCargoValueCurrency,	//45
+		// AdditionalInformation: aAdditionalInformation,
+		// HazardousMaterial: aHazardousMaterial,
+		// CustomerOrderNumber: aCustomerOrderNumber,
+	}
+
+	blAsBytes, _ := json.Marshal(bl)
+
+	return ctx.GetStub().PutState(aBLNumber, blAsBytes)
 }
 
-// QueryCar returns the car stored in the world state with given id
-func (s *SmartContract) QueryCar(ctx contractapi.TransactionContextInterface, carNumber string) (*Car, error) {
-	carAsBytes, err := ctx.GetStub().GetState(carNumber)
+// QueryBl returns the B/L stored in the world state with given id
+func (s *SmartContract) QueryBl(ctx contractapi.TransactionContextInterface, blNumber string) (*BillOfLading, error) {
+	blAsBytes, err := ctx.GetStub().GetState(blNumber)
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
 	}
 
-	if carAsBytes == nil {
-		return nil, fmt.Errorf("%s does not exist", carNumber)
+	if blAsBytes == nil {
+		return nil, fmt.Errorf("%s does not exist", blNumber)
 	}
 
-	car := new(Car)
-	_ = json.Unmarshal(carAsBytes, car)
+	bl := new(BillOfLading)
+	_ = json.Unmarshal(blAsBytes, bl)
 
-	return car, nil
+	return bl, nil
 }
 
-// QueryAllCars returns all cars found in world state
-func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
+// QueryAllBls returns all B/L found in world state
+func (s *SmartContract) QueryAllBls(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
 	startKey := ""
 	endKey := ""
 
@@ -111,41 +316,69 @@ func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface
 			return nil, err
 		}
 
-		car := new(Car)
-		_ = json.Unmarshal(queryResponse.Value, car)
+		bl := new(BillOfLading)
+		_ = json.Unmarshal(queryResponse.Value, bl)
 
-		queryResult := QueryResult{Key: queryResponse.Key, Record: car}
+		queryResult := QueryResult{Key: queryResponse.Key, Record: bl}
 		results = append(results, queryResult)
 	}
 
 	return results, nil
 }
 
-// ChangeCarOwner updates the owner field of car with given id in world state
-func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterface, carNumber string, newOwner string) error {
-	car, err := s.QueryCar(ctx, carNumber)
+/*
+
+The following block gives a first frame for planned chaincode-functions for interacting with the Bl
+--> the current "list of functions" is not completed
+
+
+func (s *SmartContract) TransferBl() error{
+
+}
+
+func (s *SmartContract) EditGrossInfo() error {
+
+}
+
+func (s *SmartContract) ReturnBl() error{
+
+}
+
+*/
+
+//changeBLOwner could be implemented with the endorsement of the BL
+
+// ChangeBlOwner updates the owner field of car with given id in world state
+/*
+
+  Taken out because of missing variable for "owner" in current state of BL-fields
+
+func (s *SmartContract) ChangeBlOwner(ctx contractapi.TransactionContextInterface, blNumber string, newOwner string) error {
+	bl, err := s.QueryBl(ctx, blNumber)
 
 	if err != nil {
 		return err
 	}
 
-	car.Owner = newOwner
+	bl.Owner = newOwner
 
-	carAsBytes, _ := json.Marshal(car)
+	blAsBytes, _ := json.Marshal(bl)
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+	return ctx.GetStub().PutState(blNumber, blAsBytes)
 }
+
+*/
 
 func main() {
 
 	chaincode, err := contractapi.NewChaincode(new(SmartContract))
 
 	if err != nil {
-		fmt.Printf("Error create fabcar chaincode: %s", err.Error())
+		fmt.Printf("Error create modified fabcar chaincode: %s", err.Error())
 		return
 	}
 
 	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting fabcar chaincode: %s", err.Error())
+		fmt.Printf("Error starting modified fabcar chaincode: %s", err.Error())
 	}
 }
--- fabcar/go/fabcar.go
+++ fabcar/go/fabcar.go
@@ -1,6 +1,16 @@
 /*
 Copyright 2020 IBM All Rights Reserved.
 
+B/L  chaincode interface program for B/L chaincode Beta Version 1.0
+
+fabcar.go main program containing some chaincode interaction commands for testing written BL chaincode locally
+
+edited for using command line arguments for chaincode interaction
+
+Edited by Malte Garmhausen
+
+eCONBiL - electronical Consignment and Bill of Lading
+
 SPDX-License-Identifier: Apache-2.0
 */
 
@@ -12,6 +22,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
@@ -61,39 +72,144 @@ func main() {
 
 	contract := network.GetContract("fabcar")
 
-	result, err := contract.EvaluateTransaction("queryAllCars")
+	// First query, output (var result) is currently cut out
+	result, err := contract.EvaluateTransaction("QueryAllBls")
 	if err != nil {
 		fmt.Printf("Failed to evaluate transaction: %s\n", err)
 		os.Exit(1)
 	}
-	fmt.Println(string(result))
+	// fmt.Println(string(result))
 
-	result, err = contract.SubmitTransaction("createCar", "CAR10", "VW", "Polo", "Grey", "Mary")
-	if err != nil {
-		fmt.Printf("Failed to submit transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
+	//the following block implements chaicode invokes and querys according to given arguments
+	if len(os.Args) > 1 {
 
-	result, err = contract.EvaluateTransaction("queryCar", "CAR10")
-	if err != nil {
-		fmt.Printf("Failed to evaluate transaction: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Println(string(result))
+		var argument string = os.Args[1]
 
-	_, err = contract.SubmitTransaction("changeCarOwner", "CAR10", "Archie")
-	if err != nil {
-		fmt.Printf("Failed to submit transaction: %s\n", err)
-		os.Exit(1)
-	}
+		if (argument == "help") || (argument == "H") || (argument == "Help") || (argument == "h") {
+			fmt.Println()
+			fmt.Println("B/L chaincode Beta Version 1.0 command line interface program")
+			fmt.Println()
+			fmt.Println("Use this program to interact with deployed chaincode")
+			fmt.Println("	--> the current chaincode beta version 1.0ß used chaincode is located in /fabric-samples/chaincode/fabcar/go/fabcar.go")
+			fmt.Println("Currently implemented functions for B/L chaincode are query, queryAll and create")
+			fmt.Println("Arguments are:")
+			fmt.Println("    queryAll")
+			fmt.Println("    query")
+			fmt.Println("    create")
+			fmt.Println("	 help | h | Help | H")
 
-	result, err = contract.EvaluateTransaction("queryCar", "CAR10")
-	if err != nil {
-		fmt.Printf("Failed to evaluate transaction: %s\n", err)
-		os.Exit(1)
+		} else if argument == "queryAll" {
+			result, err := contract.EvaluateTransaction("queryAllBls")
+			if err != nil {
+				fmt.Printf("Failed to evaluate transaction: %s\n", err)
+				os.Exit(1)
+			}
+			fmt.Println(string(result))
+
+		} else if argument == "create" {
+			if len(os.Args) == 45 {
+				// ==> pos.Args[0] = /tm/go-build... --> default while reading arguments from os input
+				// ==> os.Args[1] = "create"
+				var createArgBlNr string = os.Args[2]
+				var createArgDoi string = os.Args[3]
+				var createArgPoi string = os.Args[4]
+				var createArgNoBl string = os.Args[5]
+				var createArgShName string = os.Args[6]
+				var createArgShAddr string = os.Args[7]
+				var createArgShCntct string = os.Args[8]
+				var createArgShLF string = os.Args[9]
+				var createArgCoName string = os.Args[10]
+				var createArgCoAddr string = os.Args[11]
+				var createArgCoCntct string = os.Args[12]
+				var createArgCoLF string = os.Args[13]
+				var createArgCrName string = os.Args[14]
+				var createArgCrAddr string = os.Args[15]
+				var createArgCrCntct string = os.Args[16]
+				var createArgCrLF string = os.Args[17]
+				var createArgCrTN string = os.Args[18]
+				var createArgACName string = os.Args[19]
+				var createArgACLF string = os.Args[20]
+				var createArgACAddr string = os.Args[21]
+				var createArgNPCName string = os.Args[22]
+				var createArgNPCAddr string = os.Args[23]
+				var createArgNPCLF string = os.Args[24]
+				var createArgNPCSame string = os.Args[25]
+				var createArgIncoterms string = os.Args[26]
+				var createArgFreightCC string = os.Args[27]
+				var createArgPrpd string = os.Args[28]
+				var createArgCllct string = os.Args[29]
+				var createArgPortOfLoading string = os.Args[30]
+				var createArgPortOfDischarge string = os.Args[31]
+				var createArgPlaceOfReceipt string = os.Args[32]
+				var createArgPlaceOfDelivery string = os.Args[33]
+				var createArgOVN string = os.Args[34]
+				var createArgContNr string = os.Args[35]
+				var createArgFullContLoad string = os.Args[36]
+				var createArgLessThenContLoad string = os.Args[37]
+				var createArgDtOfRecvd string = os.Args[38]
+				var createArgShippedOnBoardDate string = os.Args[39]
+				var createArgMrksNmbrs string = os.Args[40]
+				var createArgNrOfPkg string = os.Args[41]
+				var createArgGrossWeight string = os.Args[42]
+				var createArgGrossWeightUnit string = os.Args[43]
+				var createArgDscrOfGoods string = os.Args[44]
+
+				//Output loop for testing and monitoring purposes during development
+				for i := 0; i < 45; i++ {
+					fmt.Println("[", strconv.Itoa(i), "]", " ", os.Args[i])
+				}
+				// fmt.Println(createArgGrossWeightUnit)
+				// fmt.Println(os.Args[44])
+
+				fmt.Println("Das B/L mit dem Schlüssel bzw. der BLNumber: ", createArgBlNr, " wurde im Ledger angelegt.")
+
+				result, err = contract.SubmitTransaction("createBl", createArgBlNr, createArgDoi, createArgPoi, createArgNoBl, createArgShName, createArgShAddr, createArgShCntct, createArgShLF, createArgCoName, createArgCoAddr, createArgCoCntct, createArgCoLF, createArgCrName, createArgCrAddr, createArgCrCntct, createArgCrLF, createArgCrTN, createArgACName, createArgACLF, createArgACAddr, createArgNPCName, createArgNPCAddr, createArgNPCLF, createArgNPCSame, createArgIncoterms, createArgFreightCC, createArgPrpd, createArgCllct, createArgPortOfLoading, createArgPortOfDischarge, createArgPlaceOfReceipt, createArgPlaceOfDelivery, createArgOVN, createArgContNr, createArgFullContLoad, createArgLessThenContLoad, createArgDtOfRecvd, createArgShippedOnBoardDate, createArgMrksNmbrs, createArgNrOfPkg, createArgGrossWeight, createArgGrossWeightUnit, createArgDscrOfGoods)
+				if err != nil {
+					fmt.Printf("Failed to submit transaction: %s\n", err)
+					os.Exit(1)
+				}
+				fmt.Println(string(result))
+
+			} else {
+				fmt.Println("44 Arguments expected  |  go run fabcar.go [create {BLNumber} {DateOfIssue} {PlaceOfIssue} {NrOfBLIssued} {ShipperName} {ShipperAddress} {ShipperContact} {ShipperLegalForm} {ConsigneeName} {ConsigneeAddress} {ConsigneeContact} {ConsigneeLegalForm} {CarrierName} {CarrierAddress} {CarrierContact} {CarrierLegalForm} {CarrierTrailerNr} {AgentCompanyName} {AgentCompanyAddress} {NotifyPartyCompanyName} {NotifyPartyCompanyAddress} {NotifyPartyCompanyLegalForm} {NotifyPartySameAs} {Incoterms} {FreightChargesCurrency} {Prepaid} {Collect} {PortOfLoading} {PortOfDischarge} {PlaceOfReceipt} {PlaceOfDelivery} {OceanVesselName} {Containernumber} {FullContainerLoad} {LessThenContainerLoad} {DateOfRecieved} {ShippedOnBoardDate} {MarksAndNumbers} {NumberOfPackages} {GrossWeight} {GrossWeightUnit} {DescriptionOfGoods}]")
+
+				//Output loop for testing and monitoring purposes during development
+				// for i := 0; i < 45; i++ {
+				// 	fmt.Println("[", strconv.Itoa(i), "]", " ", os.Args[i])
+				// }
+				// fmt.Println("length of args: ", len(os.Args))
+			}
+		} else if argument == "query" {
+			if len(os.Args) == 3 {
+				var queryArg string = os.Args[2]
+				result, err = contract.EvaluateTransaction("queryBl", queryArg)
+				if err != nil {
+					fmt.Printf("Failed to evaluate transaction: %s\n", err)
+					os.Exit(1)
+				}
+				fmt.Println(string(result))
+
+			} else {
+				fmt.Println("2 Arguments expected  |  go run fabcar-mod.go [query {BLNumber}]")
+			}
+			// } else if argument == "changeOwner" {
+			// 	if len(os.Args) == 4 {
+			// 		var changeArgID string = os.Args[2]
+			// 		var changeArgOw string = os.Args[3]
+			// 		_, err = contract.SubmitTransaction("changeBlOwner", changeArgID, changeArgOw)
+			// 		if err != nil {
+			// 			fmt.Printf("Failed to submit transaction: %s\n", err)
+			// 			os.Exit(1)
+			// 		}
+			// 	} else {
+			// 		fmt.Println("3 Arguments expected  |  go run fabcar-mod.go [changeOwner {BLID} {NEWBLOWNER}]")
+			// 	}
+		} else {
+			fmt.Println("No valid argument | try: queryAll , create , query, help")
+		}
+	} else {
+		fmt.Println("Missing argument(s) | try: queryAll , create , query, help")
 	}
-	fmt.Println(string(result))
 }
 
 func populateWallet(wallet *gateway.Wallet) error {
